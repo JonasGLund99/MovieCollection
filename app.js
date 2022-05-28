35b91c26943a81fce84e329542320774091bf9b7
Jonas Geertsen Lund <jgl21@student.aau.dk>
2022-05-28 20:42:50 +0200

    New update - added endpoints

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const http = require('http');
-const path = require('path');
-const port = 3000;
+const { URLSearchParams } = require('url');
+const port = 8080;
+
+const { addMovie } = require('./utils');
 
 const server = http.createServer(requestHandler);
+
 
 // function jsonResponse(res, obj){
 //     res.writeHead(200, { "Content-Type": "application/json" }),
@@ -16,6 +19,8 @@ function requestHandler(req, res) {
         processReq(req, res);
     } catch(e) {
         console.log("InternalError" + "!!: " + e);
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.write("500 - Internal server error");
     }
 }
 
@@ -25,11 +30,38 @@ function requestHandler(req, res) {
 function processReq(req, res) {
     console.log("GOT: " + req.method + " " + req.url);
     const url = req.url;
+    console.log("URL: " + url);
 
     switch (req.method) {
         case "POST": {
             switch (url) {
                 case "/add-movie": {
+                    // addMovie();
+                    req.on("data", (data) => {
+                        console.log(data);
+                        console.log(data.toString());
+                        const searchParams = new URLSearchParams(data.toString());
+                        const newMovie = {
+                            title: searchParams.get('movie-title'),
+                            personalRating: searchParams.get('personal-rating'),
+                            dateFirstWatched: searchParams.get('first-watched'),
+                            watchCounter: searchParams.get('watch-counter')
+                        }
+
+                        console.log(newMovie);
+                        // const movieDB = JSON.parse(fs.readFileSync('./movies.json'));
+                        const movieDB = require('./movies.json');
+                        movieDB.movies.push(newMovie);
+                        console.log(movieDB);
+                        const newMovieDB = JSON.stringify(movieDB, null, 4);
+                        fs.writeFileSync('./movies.json', newMovieDB);
+                        
+                        res.writeHead(302, { "location": "/"});
+                        res.end('\n')
+                    })
+                    break;
+                }
+                case "/delete-movie": {
                     
                     break;
                 }
@@ -45,23 +77,44 @@ function processReq(req, res) {
         }
         case "GET": {
             switch (url) {
-                case "/":
-                    const path = "/PublicResources/html/index.html"
-                    fs.readFileSync(path, (err, data) => {
-                        if (err) {
-                            console.log(err);
-                            res.writeHead(404, { "Content-Type": "text/html" });
-                            res.write("404 - File not found");
+                case "/": {
+                    const path = "PublicResources/html/index.html"
+                    console.log("Path: " + path);
+                    try {
+                        const data = fs.readFileSync(path, "utf8");
+                        // console.log("File content: " + data);
+                        res.writeHead(200, { "Content-Type": "text/html" });
+                        res.write(data);
+                        res.end('\n');
+                    } catch (err) {
+                        console.log(err);
+                        res.writeHead(404, { "Content-Type": "text/html" });
+                        res.write("404 - File not found");
+                        res.end('\n');  
+                    }
+                    break;
+                }
+
+                default: { //for anything else we assume it is a css or JavaScript file to be served
+                    const path = "PublicResources" + url;
+                    console.log(path);
+                    if (fs.existsSync(path)) {
+                        const data = fs.readFileSync(path);
+                        if (path.split(".").pop().toLowerCase() === "css") {
+                            res.writeHead(200, { "Content-Type": "text/css" });
                         } else {
-                            res.writeHead(200, { "Content-Type": "text/html" });
-                            res.write(data);
-                            res.end('\n');
+                            res.writeHead(200, { "Content-Type": "text/javascript" });
                         }
-                    });
+                        res.write(data);
+                        res.end('\n');
+                    } else {
+                        console.log("404 - File not found");
+                        res.writeHead(404, { "Content-Type": "text/html" });
+                        res.write("404 - File not found");
+                        res.end('\n');
+                    }
                     break;
-            
-                default:
-                    break;
+                }
             }
             break;
         }
---
+++ utils.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function addMovie() {
+    // event.preventDefault();
+    const newMovie = { };
+
+    // Input fields
+    let title = document.querySelector("#title");
+    let personalRating = document.querySelector("#p-rating");
+    let dateFirstWatched = document.querySelector("#f-watched");
+    let watchCounter = document.querySelector("#counter");
+
+    // Add new movie info
+    newMovie.title = title.value;
+    newMovie.personalRating = personalRating.value;
+    newMovie.dateFirstWatched = dateFirstWatched.value;
+    newMovie.watchCounter = watchCounter.value;
+
+    // Clear input fields
+    title.value = "";
+    personalRating.value = "";
+    dateFirstWatched.value = "";
+    watchCounter.value = "";
+
+    console.log(newMovie);
+}
+
+// function deleteMovie() {
+
+// }
+
+module.exports = { addMovie };
